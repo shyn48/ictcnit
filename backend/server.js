e93c061994ed97ec0049e8c2bd5ad51754013f56
shayan balaee <shayanfall79@gmail.com>
2020-12-12 17:51:38 +0330

    configured app entry point to run the App constructor

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+//import config from './config/index.js'
+import { dirname } from 'path'
+import { addPath } from 'app-module-path';
+
+global.__dirname = dirname(fileURLToPath(import.meta.url))
+
+addPath(__dirname)
+
+import App from './src/index.js'
+import { fileURLToPath } from 'url';
+
+//global.config = config
+
+new App()
