d7d8247dfc163c2a752331d50977981dfb8fa409
shayan balaee <shayanfall79@gmail.com>
2020-12-12 17:59:18 +0330

    configured the main App class to run the express server with the configs

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+const app = express()
+import connectDB from '../config/db.js'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import apiRouters from './routes/index.js'
+
+
+export default class App {
+    constructor() {
+        this.setConfig()
+        this.setupExpress()
+        this.connectMongo()
+        this.setRouters()
+    }
+
+    connectMongo() {
+        connectDB()
+    }
+
+    setupExpress() {
+        const PORT = process.env.PORT || 5000
+        app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+    }
+
+    setConfig() {
+        app.enable('trust proxy')
+        dotenv.config()
+    }
+
+    setRouters() {
+        app.use(apiRouters)
+    }
+}
