c21c4326bea387fc81c5d36e50be5ee6043d466e
shayan balaee <shayanfall79@gmail.com>
2020-12-20 16:44:48 +0330

    used error handling middlewares and configured Production build

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,19 @@ import connectDB from '../config/db.js'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import apiRouters from './routes/index.js'
+import { notFound, errorHandler } from './middlewares/errorHandler.js'
+import path from 'path'
+
 
 app.use(express.json())
+const __dirname = path.resolve()
 
 export default class App {
     constructor() {
         this.setConfig()
         this.setupExpress()
         this.connectMongo()
-        this.setRouters()
+        this.setRoutersAndMiddlewares()
     }
 
     connectMongo() {
@@ -27,9 +31,25 @@ export default class App {
     setConfig() {
         app.enable('trust proxy')
         dotenv.config()
+        
     }
 
-    setRouters() {
+    setRoutersAndMiddlewares() {
         app.use(apiRouters)
+        app.get('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+        if (process.env.NODE_ENV === 'production') {
+            app.uuse(express.static(path.join(__dirname, '/frontend/build')))
+
+            app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+        } else {
+            app.get('/', (req, res) => {
+                res.send('API is running...')
+            })
+        }
+
+        app.use(notFound)
+        app.use(errorHandler)
+
     }
 }
