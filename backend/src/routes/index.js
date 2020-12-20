b28f39f77095cb1c8c5b8f01dc591f7f170380b4
shayan balaee <shayanfall79@gmail.com>
2020-12-20 16:39:37 +0330

    post routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 const router = express.Router()
 
 import userRoutes from './api-v1/userRoutes.js'
+import postRoutes from './api-v1/postRoutes.js'
 
 router.use('/api/users', userRoutes)
+router.use('/api/posts', postRoutes)
 
 export default router
