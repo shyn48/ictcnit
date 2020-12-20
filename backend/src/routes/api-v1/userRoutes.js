19a4d3fc545bb17e8dabf45161336cf1c243c31a
shayan balaee <shayanfall79@gmail.com>
2020-12-20 16:43:28 +0330

    applied middlewares

--- backend/src/routes/api-v1/userRoutes.js
+++ backend/src/routes/api-v1/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 //controllers and middlewares
 import userController from '../../controllers/userController.js'
+import { admin, protect } from '../../middlewares/authMiddleWare.js'
 //import authMiddleWare from 'src/middlwares/authMiddleWare.js'
 
 
@@ -10,7 +11,7 @@ import userController from '../../controllers/userController.js'
 router.post('/create', userController.registerUser)
 router.post('/login', userController.authUser)
 
-router.route('/profile').get(userController.getUserProfile).put(userController.updateUserProfile)
-router.route('/:id').delete(userController.deleteUser).get(userController.getUserById).put(userController.updateUser)
+router.route('/profile').get(protect ,userController.getUserProfile).put(userController.updateUserProfile)
+router.route('/:id').delete(protect, admin ,userController.deleteUser).get(protect, admin ,userController.getUserById).put(protect, userController.updateUser)
 
 export default router
